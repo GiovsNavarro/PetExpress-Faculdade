581d3a6a594a1e2b3f0234613c099013924339b4
GiovsNavarro <137450592+GiovsNavarro@users.noreply.github.com>
2026-02-11 23:35:09 -0300

    Update login-js.js

--- login-js.js
+++ login-js.js
@@ -2,17 +2,12 @@ function validateForm() {
   var email = document.getElementById("email").value;
   var senha = document.getElementById("senha").value;
 
-  if (email.trim() === "") {
-    alert("Por favor, insira seu e-mail.");
+  if (email === "" || senha === "") {
+    alert("Preencha todos os campos.");
     return false;
   }
 
-  if (senha.trim() === "") {
-    alert("Por favor, insira sua senha.");
-    return false;
-  }
-
-  // Se passou na validação, redireciona manualmente
+  // redireciona manualmente
   window.location.href = "index.html";
-  return false; // impede envio real do formulário
+  return false;
 }
